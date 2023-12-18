c5caf93a9c63a4c145faa97d14182ddee8c4fab0
brendensm <brendensmithmi@gmail.com>
2023-12-18 15:05:57 -0500

    add globals

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+
+utils::globalVariables(c("V1", "V2", "coordinates", "data_value", "geolocation",
+                         "high_confidence_limit",
+                         "low_confidence_limit", "measures20", "measures21",
+                         "measures22", "measures23",
+                         "stateabbr"))
